75eae5ef013a037d3ac313e647aa38c2c8bda775
nucleartachyon <armanpanchal03.ap@gmail.com>
2023-01-27 16:33:18 +0530

    day 18

---
+++ 30 days coding streak/Day 18/script.js
@@ -0,0 +1,7 @@
+function getSize()
+{
+var w = document.documentElement.clientWidth;
+var h = document.documentElement.clientHeight;
+
+ document.getElementById('wh').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
+}
