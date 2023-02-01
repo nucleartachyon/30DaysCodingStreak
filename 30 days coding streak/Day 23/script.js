7e0eb64ac3bcb423609068caa8d54c4dcad97ecb
nucleartachyon <armanpanchal03.ap@gmail.com>
2023-02-01 20:50:59 +0530

    day23 updates

--- 30 days coding streak/Day 23/script.js
+++ 30 days coding streak/Day 23/script.js
@@ -1,20 +1,12 @@
-const twoSum = (array, goal) => {
-    let mapOfNumbers = {};
-    let twoIndexes = [];
-    for (let i = 0; i < array.length; i++) {
-        mapOfNumbers[array[i]] = i;
-    }
-    for (let i = 0; i < array.length; i++) {
-        let target = goal - array[i];
-        if (mapOfNumbers[target] !== null && mapOfNumbers[target] !== i && twoIndexes.length<2) {
-            twoIndexes.push(i);
-            twoIndexes.push(mapOfNumbers[target]);
+var twoSum = function (nums, target) {
+    for (let i = 0; i <= nums.length; i++) {
+        for (let j = 0; j <= nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j]
+            }
         }
     }
-    return twoIndexes;
 };
-let a = [1,3,10,11,14];
-
-let res = twoSum(a,13)
+let a = [1, 3, 10, 11, 14];
+let res = twoSum(a, 13)
 console.log(res);
-console.log(a.length);
